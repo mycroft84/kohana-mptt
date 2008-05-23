d4ef18be55e0abde82d589dc2624ed95e84dca67
maartenvanvliet <maartenvanvliet@f0065eb9-f742-0410-af58-5b9e58f62b86>
2008-05-23 21:30:49 +0000

    added find_file

--- Module.php
+++ Module.php
@@ -3,13 +3,16 @@
 class Module_Core{
 
 	//Array of registered modules
-	public static $modules=array();
+	protected static $modules=array();
 	
 	//Module name
 	public $module_name;
 	
 	//Module path
 	public $module_path;
+	
+	//Load all registered modules into include_paths (or not)
+	public static $load_registered_modules=false;
 	
 	//Hook for the system.post_routing event
 	public static function hook()
@@ -22,7 +25,7 @@ class Module_Core{
 	}
 	//Register new modules
 	public static function register($name,$path)
-	{
+	{	
 		self::$modules[$name]=new Module($name,$path);
 	}
 	//Register all directories in a directory as modules
@@ -49,7 +52,7 @@ class Module_Core{
 	{
 		return self::$modules[$name];
 	}	
-	//
+	
 	protected function __construct($name,$path)
 	{
 		$path=rtrim($path,'/');
@@ -64,8 +67,60 @@ class Module_Core{
 		Router::$method     = isset(Router::$rsegments[2]) ? Router::$rsegments[2] : 'index';
 		Router::$arguments  = isset(Router::$rsegments[3]) ? array_slice(Router::$rsegments, 3) : array();	
         
-		//Load triggered module into include paths so views, models and libraries can be loaded the normal way.
-		//Only current module is loaded into include path, can be altered so all registered modules are loaded
-		Config::set('core.modules',array_merge(Config::item('core.modules'),array($this->module_path)));		
+		Event::run('module.post_routing');
+		
+		$paths=array();
+		if(self::$load_registered_modules==true)
+		{
+			foreach(self::$modules as $module)
+			{
+				$paths[]=$module->module_path;
+			}
+		}
+		else
+		{
+			$paths[]=$this->module_path;
+		}
+		//Load triggered (or all) module into include paths so views, models and libraries can be loaded the normal way.
+		Config::set('core.modules',array_merge(Config::item('core.modules'),$paths));	
+
+	}
+	public function find_file($directory, $filename, $required = FALSE, $ext = FALSE, $use_cache = TRUE)
+	{
+		static $found = array();
+
+		$search = $directory.'/'.$filename;
+		$hash   = sha1($search.$ext);
+
+		if ($use_cache AND isset($found[$hash]))
+			return $found[$hash];
+					
+		if ($directory == 'config' OR $directory == 'i18n' OR $directory === 'l10n')
+		{
+			$fnd = array();
+
+			if (is_file($this->module_path.'/'.$search.EXT)) $fnd[] = $this->module_path.'/'.$search.EXT;
+
+			// If required and nothing was found, throw an exception
+			if ($required == TRUE AND $fnd === array())
+				throw new Kohana_Exception('core.resource_not_found', Kohana::lang('core.'.inflector::singular($directory)), $filename);
+
+			return $found[$hash] = $fnd;
+		}
+		else
+		{
+			// Users can define their own extensions, css, xml, html, etc
+			$ext = ($ext == FALSE) ? EXT : '.'.ltrim($ext, '.');
+
+			if (is_file($this->module_path.'/'.$search.$ext))
+				return $found[$hash] = $this->module_path.'/'.$search.$ext;
+			
+
+			// If the file is required, throw an exception
+			if ($required == TRUE)
+				throw new Kohana_Exception('core.resource_not_found', Kohana::lang('core.'.inflector::singular($directory)), $filename);
+
+			return $found[$hash] = FALSE;
+		}				
 	}
 }
