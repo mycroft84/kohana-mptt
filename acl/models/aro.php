e7b82dc251dfc44b045054f4a99b70ab597c4100
maartenvanvliet <maartenvanvliet@f0065eb9-f742-0410-af58-5b9e58f62b86>
2008-06-23 18:15:25 +0000

    added credits

--- acl/models/aro.php
+++ acl/models/aro.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Khaos :: Khacl
+ * 
+ * @author      David Cole <neophyte@sourcetutor.com>
+ * @author      dlib  <dlib@hichte.nl>
+ */
+
 class Aro_Model extends ORM{
 	protected $acts_as='mptt';
 	
--- acl/models/axo.php
+++ acl/models/axo.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Khaos :: Khacl
+ * 
+ * @author      David Cole <neophyte@sourcetutor.com>
+ * @author      dlib  <dlib@hichte.nl>
+ */
+
 class Axo_Model extends ORM{
 	
 	public function create($axo)
