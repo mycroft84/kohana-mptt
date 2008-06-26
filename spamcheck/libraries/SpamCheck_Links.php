2f6d72592359824fee769ee11a3e0da5cbf90074
maartenvanvliet <maartenvanvliet@f0065eb9-f742-0410-af58-5b9e58f62b86>
2008-06-26 10:10:10 +0000

    Link checker complete

--- spamcheck/libraries/SpamCheck_Links.php
+++ spamcheck/libraries/SpamCheck_Links.php
@@ -1,11 +1,31 @@
 <?php
 class SpamCheck_Links extends SpamCheck_Abstract{
 
-
+	protected $max_links=2;
+	
+	protected $power=2;
+	
+	public function set_max_links($count)
+	{
+		$this->max_links=$count;
+		return $this;
+	}
+	public function set_link_penalty($power)
+	{
+		$this->power=$power;
+		return $this;
+	}
 	/*
 	 * Check the comment for being spam
 	 */
 	public function check(){
-		return true;
+		$content=strtolower($this->spamcheck->get_field('content'));
+		
+		//Algorithm from Geert_DD		
+		$count = substr_count($content, 'http://');
+		$count = $count+substr_count($content, 'https://');
+		
+		return ($count < $this->max_links) ? 100 : -1*((int) pow($count, $this->power));
+		
 	}
 }
